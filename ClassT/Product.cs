883c5d01f413287585a53c4b28488804f07c4b98
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-28 17:23:52 +0400

    nn

---
+++ ClassT/Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassT
+{
+    public class Product
+    {
+        public int Id;
+
+        public string BrandName;
+
+        public string Model;
+
+        public decimal Qiymet;
+
+        public decimal Xercler;
+
+        public decimal UmumiGelir;
+
+        public int MehsulunSayi;
+
+
+        public Product(int id,string brandname,string model,decimal qiymet,decimal xercler,decimal umumigelir,int mehsulunsayi)
+        {
+            Id=id;
+            BrandName=brandname;
+            Model=model;
+            Qiymet=qiymet;
+            Xercler=xercler;
+            UmumiGelir = umumigelir;
+            MehsulunSayi=mehsulunsayi;
+           
+
+        }
+
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id - {Id}");
+            Console.WriteLine($"Marka Adi - {BrandName}");
+            Console.WriteLine($"Model - {Model}");
+            Console.WriteLine($"Qiymet - {Qiymet}");
+            Console.WriteLine($"Xercler - {Xercler}");
+            Console.WriteLine($"UmumiGelir - {UmumiGelir}");
+            Console.WriteLine($"Say - {MehsulunSayi}");
+        }
+
+
+        public void Sale(int eded)
+        {
+            if (eded <= MehsulunSayi)
+            {
+                decimal satisqiymeti = Qiymet * eded;
+                decimal Xerc = Xercler * eded;
+                UmumiGelir += satisqiymeti - Xerc;
+                MehsulunSayi -= eded;
+                Console.WriteLine($"{eded} eded mehsul satildi. UmumiGelir artirildi, Mehsulun sayi azaldildi.");
+            }
+            else
+            {
+                Console.WriteLine("Kifayət qədər məhsul yoxdur!");
+            }
+        }
+    }
+}
---
+++ ClassT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Product product = new Product(2, "Apple", "8", 100, 50, 0, 20);
+
+           
+            Console.WriteLine("Umumi Melumat:");
+            product.GetInfo();
+
+           
+            product.Sale(9);
+
+            
+            Console.WriteLine("Satisdan sonra umumi melumat:");
+            product.GetInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
